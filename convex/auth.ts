63a25e5f980344d9dcacf24b5ea1fdee0e74c09f
Saksham Tewari <sakshamtewari3@gmail.com>
2024-12-30 18:42:23 +0530

    Google Provider

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,18 +1,7 @@
 import GitHub from '@auth/core/providers/github';
+import Google from '@auth/core/providers/google';
 import { convexAuth } from '@convex-dev/auth/server';
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [
-    GitHub({
-      profile(githubProfile, tokens) {
-        return {
-          id: githubProfile.id,
-          name: githubProfile.name,
-          email: githubProfile.email,
-          image: githubProfile.picture,
-          githubId: githubProfile.id,
-        };
-      },
-    }),
-  ],
+  providers: [GitHub, Google],
 });
