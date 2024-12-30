68f5e44c98c309d25b4318100f9f37f7929cf06c
Saksham Tewari <sakshamtewari3@gmail.com>
2024-12-30 16:31:52 +0530

    handleProviderSignIn

---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,98 @@
+import { useAuthActions } from '@convex-dev/auth/react';
+import { useState } from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { SignInFlow } from '../types';
+
+// define types
+interface SignInCardProps {
+  setState: (state: SignInFlow) => void;
+}
+
+export const SignInCard = ({ setState }: SignInCardProps) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { signIn } = useAuthActions();
+  const handleProviderSignIn = (value: 'github' | 'google') => {
+    signIn(value);
+  };
+
+  return (
+    <Card className='h-full w-full p-8'>
+      <CardHeader className='px-0 pt-0'>
+        <CardTitle>Login to continue</CardTitle>{' '}
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className='space-y-5 px-0 pb-0'>
+        <form className='space-y-2.5'>
+          <Input
+            disabled={false}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='Email'
+            type='email'
+            required
+          ></Input>
+          <Input
+            disabled={false}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder='Password'
+            type='password'
+            required
+          ></Input>
+          <Button type='submit' className='w-full' size='lg' disabled={false}>
+            Continue
+          </Button>
+        </form>
+        <Separator />
+        <div className='flex flex-col gap-y-2.5'>
+          <Button
+            disabled={false}
+            onClick={() => handleProviderSignIn('google')}
+            variant='outline'
+            size='lg'
+            className='w-full relative'
+          >
+            <FcGoogle className='size-5 absolute top-3 left-2.5' />
+            Continue with Google
+          </Button>
+          <Button
+            disabled={false}
+            onClick={() => handleProviderSignIn('github')}
+            variant='outline'
+            size='lg'
+            className='w-full relative'
+          >
+            <FaGithub className='size-5 absolute top-3 left-2.5' />
+            Continue with Github
+          </Button>
+          <p
+            className='text-xs text-muted-foreground'
+            onClick={() => setState('signUp')}
+          >
+            Don't have an account?{' '}
+            <span className='text-sky-700 hover:underline cursor-pointer'>
+              Sign up
+            </span>
+          </p>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
