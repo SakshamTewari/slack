3196f38b79b86aacce079cbc4a58b6b81fec521a
Saksham Tewari <sakshamtewari3@gmail.com>
2026-07-04 20:16:53 +0530

    update: request validation

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,18 +1,14 @@
 import { FastifyInstance } from "fastify";
 
-import { AuthController } from "../controllers/auth.controller";
-import { AuthService } from "../services/auth.service";
-import { UserRepository } from "../repositories/user.repository";
+import { authController } from "../composition/auth";
 import { RegisterRequest } from "../types/auth";
+
+import { registerSchema } from "../schemas/auth.schema";
 
 export async function authRoutes(app: FastifyInstance){
     
-        const userRepository = new UserRepository();
-        const authService = new AuthService(userRepository);
-        const authController = new AuthController(authService);
-
         // POST
-        app.post<{Body: RegisterRequest}>("/auth/register", async (request, reply) => {
+        app.post<{Body: RegisterRequest}>("/register", { schema: registerSchema }, async (request, reply) => {
             return authController.register(request, reply);
         })
 };
