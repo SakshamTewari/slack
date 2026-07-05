f5e8cad6a18d29c2588771c40ef7fbbe60a0b5bf
Saksham Tewari <sakshamtewari3@gmail.com>
2026-07-05 20:09:02 +0530

    update: security config

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -2,4 +2,8 @@ import "dotenv/config";
 
 export const config = {
     port: Number(process.env.PORT) || 3000,
+
+    security: {
+        bcryptSaltRounds: Number(process.env.BCRYPT_SALT_ROUNDS) || 12,
+    }
 }
