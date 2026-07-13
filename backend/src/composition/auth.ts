7c9d15aa6fe56afa6f0544f8b4567465be7fe2e9
Saksham Tewari <sakshamtewari3@gmail.com>
2026-07-13 21:54:10 +0530

    update: auth composite

--- backend/src/composition/auth.ts
+++ backend/src/composition/auth.ts
@@ -1,8 +1,20 @@
+import { FastifyInstance } from "fastify";
+
 import { AuthController } from "../controllers/auth.controller";
 import { AuthService } from "../services/auth.service";
 import { UserRepository } from "../repositories/user.repository";
+import { JWTService } from "../services/jwt.service";
 
-const userRepository = new UserRepository();
-const authService = new AuthService(userRepository);
+export default function auth(app: FastifyInstance) {
+    const userRepository = new UserRepository();
+    
+    const jwtService = new JWTService(app);
 
-export const authController = new AuthController(authService);
+    const authService = new AuthService(userRepository, jwtService);
+
+    const authController = new AuthController(authService);
+
+    return{
+        authController,
+    }
+}
